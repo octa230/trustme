db165409c2e9d68dc99c66c4740c9316c0776d2d
octa230 <mervynstunner@outlook.com>
2025-05-17 17:16:51 +0400

    ui

--- api/models/department.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const departmentSchema = mongoose.Schema({
-    name: {type: String},
-    controlId: {type: String}
-})
--- api/services/address/server.js
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express'
-
-const addressRouter = Router()
-
-
-
-export default addressRouter
